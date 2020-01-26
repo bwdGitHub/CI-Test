2bba15f9689c2e10f40f7ed920e453e2eeccd9e7
Ben Wells-Day <benwellsday@gmail.com>
2020-01-26 02:29:02 +0000

    directory confusion and python command line confusion

--- test_foo.py
+++ test_foo.py
@@ -3,7 +3,7 @@ import sys
 # todo - this path should work for the CI
 # but doesn't match my structure. Is there a way to support both?
 # or should I just keep parity between my local workspace and the CI version (would make more sense...)
-sys.path.append("../src/")
+sys.path.append("src/")
 import foo
 
 class TestFoo(unittest.TestCase):
